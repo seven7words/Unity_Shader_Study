d3af593ec05377a17551a3752cfa8ecd80a37a92
woopqww111 <woopqww111@yahoo.com>
2017-12-06 15:00:54 +0800

    第八章全部OK

---
+++ New Unity Project/Assets/Editor/ChangeLighting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public  class ChangeLighting : Editor {
+    #region 常量
+    #endregion
+    #region  属性
+    #endregion
+    #region 字段
+    #endregion
+    #region 事件
+    #endregion
+    #region 方法
+    #endregion
+    #region Unity回调
+    [MenuItem("自己的插件/关掉天空盒")]
+    static void CloseSkyBox(){
+        RenderSettings.skybox = null;
+    }
+    #endregion
+    #region  事件回调
+    #endregion
+    #region 帮助方法
+    #endregion
+	
+}
